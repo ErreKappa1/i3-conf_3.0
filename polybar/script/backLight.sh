484f5413e1b8a7613e125314805472c6d56eda57
ErreKappa1 <riccardo.oglietti@tutanota.com>
2023-01-08 18:39:22 +0100

    Fixed backlight problem

--- polybar/script/backLight.sh
+++ polybar/script/backLight.sh
@@ -1,7 +1,7 @@
 #! /bin/zsh
 
-act=$(cat /sys/class/backlight/intel_backlight/actual_brightness)
-max=$(cat /sys/class/backlight/intel_backlight/max_brightness)
+act=$(cat /sys/class/backlight/acpi_video0/actual_brightness)
+max=$(cat /sys/class/backlight/acpi_video0/max_brightness)
 
 let act1=$act\*100
 let perc=$act1/$max
